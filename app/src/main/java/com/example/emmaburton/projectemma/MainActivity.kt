16a3857f0e5fbe00dc6e37ad1118027b4d2ae5b2
Emma Burton <emma.burton@asos.com>
2018-11-11 18:20:32 +0000

    start of MainActivity

--- app/src/main/java/com/example/emmaburton/projectemma/MainActivity.kt
+++ app/src/main/java/com/example/emmaburton/projectemma/MainActivity.kt
@@ -2,13 +2,39 @@ package com.example.emmaburton.projectemma
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var searchEditText: EditText
+    private lateinit var searchText: String
+    private lateinit var searchButton: Button
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
 
-        super.onCreate(savedInstanceState)
+        searchEditText = findViewById(R.id.search_edit_text)
+        searchButton = findViewById(R.id.search_button)
+
+        searchButtonClick(searchButton)
+
     }
+
+    fun searchButtonClick(view: View) {
+        view.setOnClickListener {
+
+            @Override
+            fun onClick() {
+                searchText = searchEditText.text.toString()
+            }
+
+            println("Searching for $searchText")
+
+        }
+
+    }
+
 }
