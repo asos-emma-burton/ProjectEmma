ff23609c2ba37e8d527a01bfc2bf5024d9fddbd2
Emma Burton <emma.burton@asos.com>
2018-11-11 12:16:57 +0000

    More setup

--- app/src/main/java/com/example/emmaburton/projectemma/MainActivity.kt
+++ app/src/main/java/com/example/emmaburton/projectemma/MainActivity.kt
@@ -2,17 +2,11 @@ package com.example.emmaburton.projectemma
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Window
-import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
-
-        //Set content view AFTER removing title/notification bar to avoid crash
         this.setContentView(R.layout.activity_main)
 
         super.onCreate(savedInstanceState)
