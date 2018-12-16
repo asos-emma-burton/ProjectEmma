e1b984ffd2ae26d2a0b4278ec89f10c2dd56ef11
Emma Burton <emma.burton@asos.com>
2018-12-16 17:27:03 +0000

    Suppress unused warning

--- app/src/main/java/com/example/emmaburton/projectemma/presentation/activity/RecipesApplication.kt
+++ app/src/main/java/com/example/emmaburton/projectemma/presentation/activity/RecipesApplication.kt
@@ -3,6 +3,7 @@ package com.example.emmaburton.projectemma.presentation.activity
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
 
+@Suppress("unused")
 class RecipesApplication : Application() {
 
     override fun onCreate() {
