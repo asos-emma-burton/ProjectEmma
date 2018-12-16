6f233e9a1d78e0c4ae24c6a64f7d778e7e190848
Emma Burton <emma.burton@asos.com>
2018-12-16 17:25:34 +0000

    Remove unnecessary serialisation in Recipe class

--- app/src/main/java/com/example/emmaburton/projectemma/data/model/Recipe.kt
+++ app/src/main/java/com/example/emmaburton/projectemma/data/model/Recipe.kt
@@ -1,37 +1,10 @@
 package com.example.emmaburton.projectemma.data.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Recipe(
-
-        @SerializedName("name") val name: String,
-
-        @SerializedName("ingredients") val ingredients: List<Ingredient>,
-
-        @SerializedName("steps") val steps: Array<String>,
-
-        @SerializedName("timers") val timers: Array<Int>,
-
-        @SerializedName("imageURL") val imageURL: String,
-
-        @SerializedName("originalURL") val originalURL: String
-
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Recipe
-
-        if (!steps.contentEquals(other.steps)) return false
-        if (!timers.contentEquals(other.timers)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = steps.contentHashCode()
-        result = 31 * result + timers.contentHashCode()
-        return result
-    }
-}
+        val name: String,
+        val ingredients: List<Ingredient>,
+        val steps: List<String>,
+        val timers: List<Int>,
+        val imageURL: String,
+        val originalURL: String
+)
